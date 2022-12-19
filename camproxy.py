8e54ead8ef0f8bb28f91c52320fcd33b7fb9f265
Steve Richardson <tangentaudio@gmail.com>
2022-12-19 12:00:33 -0500

    fixes to remove google plus nonsense

--- camproxy.py
+++ camproxy.py
@@ -6,11 +6,16 @@ import flask
 from flask import Response
 from apiclient.discovery import build
 from oauth2client import client
+import base64
 
 Config = ConfigParser.ConfigParser()
 Config.read('camproxy.ini')
-MonitorURLFormatFull = Config.get('Zoneminder', 'MonitorURLFormatFull')
-MonitorURLFormatHalf = Config.get('Zoneminder', 'MonitorURLFormatHalf')
+ZoneMinderMonitorURLFormatFull = Config.get('Zoneminder', 'MonitorURLFormatFull')
+ZoneMinderMonitorURLFormatHalf = Config.get('Zoneminder', 'MonitorURLFormatHalf')
+NXMonitorURLFormatFull = Config.get('NXWitness', 'MonitorURLFormatFull')
+NXMonitorURLFormatHalf = Config.get('NXWitness', 'MonitorURLFormatHalf')
+NXUser = Config.get('NXWitness', 'User')
+NXPassword = Config.get('NXWitness', 'Password')
 RequiredDomain = Config.get('Authentication', 'RequiredDomain')
 ClientSecretsFile = Config.get('Authentication', 'ClientSecretsFile')
 FlaskHost = Config.get('Flask', 'Host')
@@ -40,24 +45,42 @@ def index(cam, rando, fullsize):
     return flask.redirect(flask.url_for('oauth2callback'))
   else:
     http_auth = credentials.authorize(httplib2.Http())
-    service = build('plus', 'v1', http=http_auth)
+    service = build('people', 'v1', http=http_auth)
 
     if service:
       ppl = service.people()
-      ppl_doc = ppl.get(userId='me').execute()
+      ppl_doc = ppl.get(resourceName='people/me',personFields='names,organizations,memberships,emailAddresses,photos').execute()
 
-      if 'domain' in ppl_doc:
-        if ppl_doc['domain'] == RequiredDomain:
+      #print ppl_doc
+      
+      if 'emailAddresses' in ppl_doc:
+        #if ppl_doc['domain'] == RequiredDomain:
+        if '@'+RequiredDomain in ppl_doc['emailAddresses'][0]['value']:
           if cam:
-            if fullsize:
-              URL = MonitorURLFormatFull % (cam)
+            if '-' in cam:
+              if fullsize:
+                URL = NXMonitorURLFormatFull % (cam)
+              else:
+                URL = NXMonitorURLFormatHalf % (cam)
+
+              h = httplib2.Http()
+              auth = base64.encodestring(NXUser + ':' + NXPassword);
+              resp, content = h.request(URL, 'GET', headers = { 'Authorization' : 'Basic ' + auth })
+
+              return Response(content, mimetype='image/jpeg')
+
             else:
-              URL = MonitorURLFormatHalf % (cam)
-  
-            resp, content = httplib2.Http().request(URL)
-            return Response(content, mimetype='image/jpeg')
+              if fullsize:
+                URL = ZoneMinderMonitorURLFormatFull % (cam)
+              else:
+                URL = ZoneMinderMonitorURLFormatHalf % (cam)
+
+              resp, content = httplib2.Http().request(URL)
+                
+              return Response(content, mimetype='image/jpeg')
+
           else:
-            return flask.render_template('cams.html', username=ppl_doc['displayName'], userimage=ppl_doc['image']['url'])
+            return flask.render_template('cams.html', username=ppl_doc['names'][0]['displayName'], userimage=ppl_doc['photos'][0]['url'])
       else:
         return "Please use your %s account.  You will need to clear a cookie and reload this page to relogin (known bug)." % (RequiredDomain)
         
@@ -65,6 +88,7 @@ def index(cam, rando, fullsize):
       return "Can't build service."
 
     return "You are not a %s member." % (RequiredDomain)
+
 
 @app.route('/oauth2callback')
 def oauth2callback():
@@ -77,13 +101,17 @@ def oauth2callback():
     return flask.redirect(auth_uri)
   else:
     auth_code = flask.request.args.get('code')
-    credentials = flow.step2_exchange(auth_code)
+    try:
+      credentials = flow.step2_exchange(auth_code)
+    except client.FlowExchangeError as exchange_error:
+        return "An error has occurred: {0}".format(exchange_error)
+      
     flask.session['credentials'] = credentials.to_json()
     return flask.redirect(flask.url_for('index'))
 
-@app.route('/favico.ico')
+@app.route('/favicon.ico')
 def favicon():
-  return
+  return Response('', mimetype='image/bmp')
 
 if __name__ == '__main__':
   app.debug = DEBUG
