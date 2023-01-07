c928114fb613e00efdb8777ea6422684bc2c507c
Steve Richardson <tangentaudio@gmail.com>
2023-01-07 14:02:21 -0500

    fix fresh load with no stored selected device/area/page.  improve thumb loading indication.

--- react-frontend/src/components/Home.jsx
+++ react-frontend/src/components/Home.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import * as React from 'react';
+import { useEffect, useState, useRef } from "react";
 import { BACKEND_URL } from "../App";
 import Axios from "axios";
 import { useNavigate } from "react-router";
@@ -8,9 +9,7 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import LogoutIcon from '@mui/icons-material/Logout';
-import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -21,28 +20,45 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from "@mui/material/ListSubheader";
 import Grid from '@mui/material/Grid';
-import CameraIcon from '@mui/icons-material/VideoCameraFrontOutlined';
-import AreaIcon from '@mui/icons-material/Handyman';
+import MenuIcon from '@mui/icons-material/Menu';
+import CameraIcon from '@mui/icons-material/CropOriginalSharp';
+import AreaIcon from '@mui/icons-material/GridOnSharp';
 import Thumb from "./Thumb";
-
 import '@fontsource/roboto/300.css';
 
-const pages = ['Single Camera', 'Multi Camera'];
 
-function Home() {
+interface Props {
+	window?: () => Window;
+}
+
+const PAGE_SINGLE_CAM = 'Single';
+const PAGE_MULTI_CAM = 'Multi'
+
+function Home(props: Props) {
+	const { window } = props;
+	const [mobileOpen, setMobileOpen] = useState(false);
 
 	const [auth, setAuth] = useState(null);
 	const [devices, setDevices] = useState({});
 	const [areasToIds, setAreasToIds] = useState({});
 	const [selectedDevice, setSelectedDevice] = useState(null);
 	const [selectedArea, setSelectedArea] = useState(null);
-	const [selectedPage, setSelectedPage] = useState('Single Camera')
+	const [selectedPage, setSelectedPage] = useState(PAGE_SINGLE_CAM)
+	
+	const elementRef = useRef(null);
+	const [mainWidth, setMainWidth]=useState(0);
+	
 	const nav = useNavigate()
 
 	const drawerWidth = 240;
 
+
+	const handleDrawerToggle = () => {
+		setMobileOpen(!mobileOpen);
+	};
+
 	const lookupDevice = (id) => {
-		for (var i=0; i<devices.length; i++) {
+		for (var i = 0; i < devices.length; i++) {
 			if (devices[i].id === id)
 				return devices[i].name;
 		}
@@ -69,10 +85,12 @@ function Home() {
 	}
 
 	useEffect(() => {
-		
-		setSelectedArea(getLocalItem("selectedArea", selectedArea))
-		setSelectedDevice(getLocalItem("selectedDevice", selectedDevice))
-		setSelectedPage(getLocalItem("selectedPage", selectedPage))
+		let w=elementRef.current.getBoundingClientRect().width;
+		if (w>1280)
+			w=1280;
+		setMainWidth(w);
+
+
 
 		if (localStorage.getItem('JWT') == null) {
 			return nav("/login")
@@ -92,8 +110,11 @@ function Home() {
 				const devs = res.data
 				setDevices(devs)
 
+				// Build area list and area/ID map from device list
+				// Device names must be formatted as "Area - Name"
 				let areas = {};
-				for (var i=0; i<devs.length; i++) {
+				let arealist = [];
+				for (var i = 0; i < devs.length; i++) {
 					let n = devs[i].name;
 					let s = n.split('-');
 					if (s.length >= 2) {
@@ -104,13 +125,28 @@ function Home() {
 						}
 						idlist.push(devs[i].id);
 						areas[area] = idlist;
+						arealist.push(area);
 					}
 				}
 				setAreasToIds(areas);
+
+				// pick the first device if none stored
+				let selDev = getLocalItem("selectedDevice", devs[0].id);
+				setSelectedDevice(selDev);
+
+				// pick the first area if none stored
+				let selArea = getLocalItem("selectedArea", arealist[0]);
+				setSelectedArea(selArea);
+
+	
 			})
 			.catch((err) => console.log(err));
 
-	}, [nav, setDevices])
+		let selPage = getLocalItem("selectedPage", PAGE_SINGLE_CAM);
+		setSelectedPage(selPage);
+	
+
+	}, [nav, setDevices, elementRef])
 
 	const handleLogout = () => {
 		localStorage.removeItem('JWT')
@@ -120,33 +156,31 @@ function Home() {
 	const handleDeviceClick = (event, id) => {
 		setSelectedDevice(id);
 		localStorage.setItem("selectedDevice", id);
+		setMobileOpen(false);
 	}
 
 	const handleAreaClick = (event, area) => {
 		setSelectedArea(area);
 		localStorage.setItem("selectedArea", area);
+		setMobileOpen(false);
 	}
 
 	const handleMultiClick = (event, id) => {
 		// jump to single view of this camera
 		setSelectedDevice(id);
 		localStorage.setItem("selectedDevice", id);
-		setPage('Single Camera');
+		setPage(PAGE_SINGLE_CAM);
 	}
 
 	const handleSingleClick = (event, id) => {
 		// find the area that contains this camera and show multi-view
-		
+
 		for (const area in areasToIds) {
-			console.log(area);
 			const ids = areasToIds[area];
-			console.log("looking for " + id);
-			console.log(ids);
 			if (ids.includes(id)) {
-				console.log("found");
 				setSelectedArea(area);
 				localStorage.setItem("selectedArea", area);
-				setPage('Multi Camera');
+				setPage(PAGE_MULTI_CAM);
 				return;
 			}
 		}
@@ -155,22 +189,22 @@ function Home() {
 
 	const drawerContents = (
 		<div>
-			{ selectedPage === "Single Camera" &&
+			{selectedPage === PAGE_SINGLE_CAM &&
 				<Box>
 					<Typography variant="button" sx={{ marginLeft: 2, fontWeight: 'bold', color: 'blue' }}>Available Cameras</Typography>
 					<List dense>
 						{
 							Object.keys(areasToIds).map((item) => (
 								<>
-								<ListSubheader sx={{color: 'green', fontWeight: 'bold', backgroundColor: '#eeeeee' }}>{item}</ListSubheader>
-								{areasToIds[item].map( (dev) => (
-									<ListItem key={dev} disablePadding selected={ selectedDevice === dev}>
-										<ListItemButton onClick={(event) => handleDeviceClick(event, dev)} >
-										<ListItemIcon sx={{minWidth: '12px', paddingRight: '2px', opacity: '50%'}}><CameraIcon/></ListItemIcon>
-											<ListItemText primary={lookupDevice(dev).split(' - ')[1]}/>
-										</ListItemButton>
-									</ListItem>
-								))}
+									<ListSubheader sx={{ color: 'green', fontWeight: 'bold', backgroundColor: '#eeeeee' }}>{item}</ListSubheader>
+									{areasToIds[item].map((dev) => (
+										<ListItem key={dev} disablePadding selected={selectedDevice === dev}>
+											<ListItemButton onClick={(event) => handleDeviceClick(event, dev)} >
+												<ListItemIcon sx={{ minWidth: '12px', paddingRight: '2px', opacity: '50%' }}><CameraIcon /></ListItemIcon>
+												<ListItemText primary={lookupDevice(dev).split(' - ')[1]} />
+											</ListItemButton>
+										</ListItem>
+									))}
 								</>
 							))
 
@@ -180,16 +214,16 @@ function Home() {
 				</Box>
 			}
 
-			{ selectedPage === "Multi Camera" &&
+			{selectedPage === PAGE_MULTI_CAM &&
 				<Box>
 					<Typography variant="button" sx={{ marginLeft: 2, fontWeight: 'bold', color: 'blue' }}>Available Areas</Typography>
 					<List>
 						{
 							Object.keys(areasToIds).map((item) => (
-								<ListItem key={item} disablePadding selected={ selectedArea === item }>
+								<ListItem key={item} disablePadding selected={selectedArea === item}>
 									<ListItemButton onClick={(event) => handleAreaClick(event, item)} >
-									<ListItemIcon><AreaIcon/></ListItemIcon>
-										<ListItemText primary={item} secondary={areasToIds[item].length + " cameras"}/>
+										<ListItemIcon><AreaIcon /></ListItemIcon>
+										<ListItemText primary={item} secondary={areasToIds[item].length + " cameras"} />
 									</ListItemButton>
 								</ListItem>
 							))
@@ -197,108 +231,132 @@ function Home() {
 
 					</List>
 				</Box>
-			}				
+			}
 		</div>
 	)
 
 	const container = window !== undefined ? () => window().document.body : undefined;
 
 	return (
-		<Box sx={{ display: 'flex' }}>
+		<Box sx={{ display: 'flex' }} >
 			<CssBaseline />
 			<Box>
-				<AppBar position="fixed" sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }} component="nav">
-					<Container maxWidth="xl">
-						<Toolbar disableGutters>
-							<Typography
-								variant="h6"
-								noWrap
-								component="a"
-								sx={{
-									mr: 2,
-									display: { xs: 'none', md: 'flex' },
-									fontFamily: 'sans-serif',
-									fontWeight: 700,
-									letterSpacing: '-.05rem',
-									color: 'orange',
-									textDecoration: 'none',
-								}}
-							>
-								MakeIt Labs
-							</Typography>
+				<AppBar
+					position="fixed"
+					sx={{
+						width: { sm: `calc(100% - ${drawerWidth}px)` },
+						ml: { sm: `${drawerWidth}px` },
+					}}
+					ref={elementRef}
 
-							<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-								{pages.map((page) => (
-									<Button
-										key={page}
-										sx={{ color: 'white' }}
-										onClick={ () => { setPage(page) } }
-									>
-										{page}
-									</Button>
-								))}
-							</Box>
+				>
+					<Toolbar>
 
-							<Box sx={{ flexGrow: 0, marginRight: "10px" }}>
-								<Tooltip title={auth ? auth["email"] : ""}><Typography variant="button">{auth ? auth["name"] : ""}</Typography></Tooltip>
-							</Box>
+						<IconButton
+							color="inherit"
+							aria-label="open drawer"
+							edge="start"
+							onClick={handleDrawerToggle}
+							sx={{ mr: 2, display: { sm: 'none' } }}
+						>
+							<MenuIcon />
+						</IconButton>
 
-							<Box sx={{ flexGrow: 0 }}>
-								<IconButton sx={{ p: 0 }}>
-									<Avatar alt="Avatar" src={auth ? auth["picture"] : ""} />
+						<Typography
+							variant="h6"
+							noWrap
+							component="a"
+							sx={{
+								mr: 2,
+								display: { xs: 'none', md: 'flex' },
+								fontFamily: 'sans-serif',
+								fontWeight: 700,
+								letterSpacing: '-.05rem',
+								color: 'orange',
+								textDecoration: 'none',
+							}}
+						>
+							MakeIt Labs
+						</Typography>
+
+						<Box sx={{ flexGrow: 1, display: { md: 'flex' } }}>
+							<IconButton key={PAGE_SINGLE_CAM} sx={{color:'white'}} onClick={() => {setPage(PAGE_SINGLE_CAM)}}><CameraIcon/></IconButton>
+							<IconButton key={PAGE_MULTI_CAM} sx={{color:'white'}} onClick={() => {setPage(PAGE_MULTI_CAM)}}><AreaIcon/></IconButton>
+						</Box>
+
+						<Box sx={{ flexGrow: 0, display: { xs: 'none'}, marginRight: "10px" }}>
+							<Tooltip title={auth ? auth["email"] : ""}><Typography variant="button">{auth ? auth["name"] : ""}</Typography></Tooltip>
+						</Box>
+
+						<Box sx={{ flexGrow: 0 }}>
+							<IconButton sx={{ p: 0 }}>
+								<Avatar alt={auth ? auth["name"] : ""} src={auth ? auth["picture"] : ""} />
+							</IconButton>
+
+							<Tooltip title="Logout">
+								<IconButton onClick={() => handleLogout()} color="inherit">
+									<LogoutIcon />
 								</IconButton>
+							</Tooltip>
 
-								<Tooltip title="Logout">
-									<IconButton onClick={() => handleLogout()} color="inherit">
-										<LogoutIcon />
-									</IconButton>
-								</Tooltip>
+						</Box>
 
-							</Box>
+					</Toolbar>
 
-						</Toolbar>
-
-
-					</Container>
 				</AppBar>
 			</Box>
 
-			<Box component="nav">
+			<Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
 				<Drawer
-					sx={{
-						width: drawerWidth,
-						flexShrink: 0,
-						'& .MuiDrawer-paper': {
-							width: drawerWidth,
-							boxSizing: 'border-box',
-						},
+					container={container}
+					variant="temporary"
+					open={mobileOpen}
+					onClose={handleDrawerToggle}
+					ModalProps={{
+						keepMounted: true, // Better open performance on mobile.
 					}}
+					sx={{
+						display: { xs: 'block', sm: 'none' },
+						'& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+					}}
+				>
+					{drawerContents}
+				</Drawer>
+				<Drawer
 					variant="permanent"
-					anchor="left"
+					sx={{
+						display: { xs: 'none', sm: 'block' },
+						'& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+					}}
+					open
 				>
 					{drawerContents}
 				</Drawer>
 			</Box>
-			{ selectedPage === "Single Camera" &&
-				<Box component="main" sx={{ p: 3 }}>
+
+
+			{selectedPage === PAGE_SINGLE_CAM &&
+				<div>
+					<Box component="main" sx={{ flexGrow: 1 }}>
+						<Toolbar />
+						<Thumb id={selectedDevice} name={lookupDevice(selectedDevice)} width={mainWidth - 20} interval="1500" clickCallback={handleSingleClick} user={auth ? auth.email : ""} ></Thumb>
+					</Box>
+				</div>
+			}
+			{selectedPage === PAGE_MULTI_CAM &&
+				<Box component="main" sx={{ flexGrow: 1 }}>
 					<Toolbar />
-					<Thumb id={selectedDevice} name={lookupDevice(selectedDevice) } height="720" interval="1500" clickCallback={handleSingleClick}></Thumb>
-				</Box>
-			}	
-			{ selectedPage === "Multi Camera" &&
-				<Box component="main">
-					<Toolbar />
-					<Grid container direction="row" alignItems="flex-start" justifyItems="flex-start" rowSpacing="4px" columnSpacing="4px" >
-							{
-								Object.values(getSelectedAreaIds(selectedArea)).map((item) => (
-									<Grid item >
-										<Thumb id={item} name={lookupDevice(item) } height="220" interval="3000" hideTime clickCallback={handleMultiClick}></Thumb>
-									</Grid>
-								))
-							}
+					<Grid container alignItems="left" justifyContent="left" rowSpacing="1px" columnSpacing="1px" sx={{width: mainWidth - 20}}>
+						{
+							Object.values(getSelectedAreaIds(selectedArea)).map((item) => (
+								<Grid item sx={{ minWidth: 320, justifyContent: 'center' }}>
+									<Thumb id={item} name={lookupDevice(item)} width="320" interval="3000" hideTime clickCallback={handleMultiClick} user={auth ? auth.email : ""}></Thumb>
+								</Grid>
+							))
+						}
 					</Grid>
 				</Box>
-			}	
+			}
 		</Box>
 	);
 }
--- react-frontend/src/components/Thumb.jsx
+++ react-frontend/src/components/Thumb.jsx
@@ -2,18 +2,30 @@ import React, { useEffect, useState } from "react";
 import { BACKEND_URL } from "../App";
 import Axios from "axios";
 import { useNavigate } from "react-router";
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
 
-function Thumb({ id, name, height, interval, hideTime, clickCallback }) {
+function Thumb({ id, name, height, width, interval, hideTime, clickCallback, user }) {
     const [auth, setAuth] = useState(null);
     const [thumb, setThumb] = useState(null);
     const nav = useNavigate()
 
     const getThumb = (id) => {
         if (id !== null && id !== "" && id !== undefined) {
-            Axios.get(`${BACKEND_URL}/thumbnail?camera_id=${id}&height=${height}`, {
+            let u = user;
+            if (u === undefined || u === null || u === "") {
+                u = "unknown";
+            }
+            
+            let url="";
+            if (height !== undefined) {
+                url=`${BACKEND_URL}/thumbnail?camera_id=${id}&height=${height}&user=${u}`;
+            } else if (width !== undefined) {
+                url=`${BACKEND_URL}/thumbnail?camera_id=${id}&width=${width}&user=${u}`;
+            }
+            Axios.get(url, {
                 responseType: 'blob',
                 headers: { "Authorization": `Bearer ${localStorage.getItem('JWT')}` }
             })
@@ -27,19 +39,6 @@ function Thumb({ id, name, height, interval, hideTime, clickCallback }) {
     useEffect(() => {
         getThumb(id);
 
-        /*
-		if (localStorage.getItem('JWT') == null) {
-			return nav("/login")
-		}
-		else {
-			Axios.get(`${BACKEND_URL}/home`, { headers: { "Authorization": `Bearer ${localStorage.getItem('JWT')}` } })
-				.then((res) => {
-					setAuth(res)
-				})
-				.catch((err) => console.log(err));
-		}
-        */
-
         const timer = setInterval(() => {
             getThumb(id);
         }, interval);
@@ -50,29 +49,56 @@ function Thumb({ id, name, height, interval, hideTime, clickCallback }) {
 
     return (
         <Box>
-            <Button onClick={ (event) => { if (clickCallback !== undefined) { clickCallback(event, id); console.log(id) }} }><img src={thumb} alt="" /></Button>
-            <Box sx={{
-                position: 'relative',
-                bottom: 40,
-                left: 0,
-                width: '100%',
-                marginLeft: 2,
-                textShadow: '1px 3px 3px black',
-            }}>
-                <Typography variant="h8" color="white">{name}</Typography>
+            { thumb === null &&
 
-                { hideTime !== true &&
-                    <Box sx={{
-                        position: 'relative',
-                        bottom: height - 20,
-                        right: 0,
-                        width: '100%',
-                        textShadow: '1px 2px 2px black',
-                    }}>
-                        <Typography variant="h8" color="white">{now}</Typography>
-                    </Box>
-}
-            </Box>
+                <Box sx={{ backgroundColor: '#eeeeee', width: width, height: (width*9/16), display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <CircularProgress disableShrink>Image loading...</CircularProgress>
+                    <Typography variant="h6" sx={{marginLeft: '10px'}}>Loading...</Typography>
+                </Box>
+
+            }
+            { thumb !== null &&
+                <Box sx={{ width: '100%', height: '100%', backgroundColor: '#aaaaaa', textAlign: 'center'}} >
+                    <Button sx={{ p: 0, m: 0}} onClick={ (event) => { if (clickCallback !== undefined) { clickCallback(event, id) }} }>
+                        <img src={thumb} alt="" />
+                    { hideTime === true &&
+                        <Box sx={{
+                            position: 'absolute',
+                            bottom: 0,
+                            right: 0,
+                            
+                            textShadow: '1px 3px 3px black',
+                            backgroundColor: 'transparent',
+                            fontWeight: 'bolder',
+                            color: 'white',
+                            padding: '4px',
+                            margin: 0
+                        }}>
+                            <Box sx={{ justifyContent:'right' }}>
+                                <div>{name} </div> 
+                            </Box>
+                        </Box>
+                    }
+                    { hideTime !== true &&
+                        <Box width="100%" sx={{
+                            position: 'absolute',                                                
+                            bottom: 0,
+                            textShadow: '1px 3px 3px black',
+                            backgroundColor: 'black',
+                            fontWeight: 'bolder',
+                            color: 'white',
+                            padding: '4px',
+                            margin: 0
+                        }}>
+                            <Box>
+                                <div>{name} {now}</div>
+                            </Box>
+                        </Box>
+                    }
+
+                </Button>
+                </Box>
+            }
         </Box>
     );
 
